32e18c38696b7ce34e5a9505a9377a60c696d017
denpamusic <denpa@denpa.pro>
2022-07-10 04:55:07 +0300

    Add connection getter.

--- custom_components/plum_ecomax/entity.py
+++ custom_components/plum_ecomax/entity.py
@@ -28,32 +28,37 @@ class EcomaxEntity(ABC):
     @property
     def device(self):
         """Return device object."""
-        return self._connection.device
+        return self.connection.device
 
     @property
     def available(self) -> bool:
         """Indicates whether the entity is available."""
-        return self._connection.device is not None
+        return self.connection.device is not None
 
     @property
     def device_info(self) -> DeviceInfo:
         """Return device info."""
-        return self._connection.device_info
+        return self.connection.device_info
 
     @property
     def entity_registry_enabled_default(self) -> bool:
         """Indicate if the entity should be enabled when first added."""
-        return self.entity_description.key in self._connection.capabilities
+        return self.entity_description.key in self.connection.capabilities
 
     @property
     def unique_id(self) -> str:
         """A unique identifier for this entity."""
-        return f"{self._connection.uid}-{self.entity_description.key}"
+        return f"{self.connection.uid}-{self.entity_description.key}"
 
     @property
     def name(self) -> str:
         """Name of the entity."""
-        return f"{self._connection.name} {self.entity_description.name}"
+        return f"{self.connection.name} {self.entity_description.name}"
+
+    @property
+    def connection(self) -> EcomaxConnection:
+        """Ecomax connection instance."""
+        return self._connection
 
     @property
     def should_poll(self) -> bool:
