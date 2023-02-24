479bcc4d6f9f83bfbeb45fce8f607d4314616259
denpamusic <denpa@denpa.pro>
2023-02-24 23:50:25 +0300

    Generalize device data conversion.

--- custom_components/plum_ecomax/diagnostics.py
+++ custom_components/plum_ecomax/diagnostics.py
@@ -7,36 +7,21 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from pyplumio import __version__ as pyplumio_version
 from pyplumio.devices import Device
+from pyplumio.helpers.event_manager import EventManager
 
-from .const import (
-    ATTR_MIXERS,
-    ATTR_PASSWORD,
-    ATTR_PRODUCT,
-    ATTR_REGDATA,
-    ATTR_THERMOSTATS,
-    CONF_HOST,
-    CONF_UID,
-    DOMAIN,
-)
+from .const import ATTR_PASSWORD, ATTR_PRODUCT, CONF_HOST, CONF_UID, DOMAIN
 
 REDACTED: Final = "**REDACTED**"
 
 
-def _sub_devices_as_dict(device_data: dict[str, Any]) -> dict[str, Any]:
-    """Represent sub devices in the device data as dictionaries."""
-    for sub_device in (ATTR_MIXERS, ATTR_THERMOSTATS):
-        if sub_device in device_data:
-            device_data[sub_device] = {
-                x: y.data for x, y in device_data[sub_device].items()
-            }
+def _device_data_as_dict(device_data: dict[str, Any]) -> dict[str, Any]:
+    """Represent device data as dictionary."""
+    for key, value in device_data.items():
+        if isinstance(value, dict):
+            device_data[key] = _device_data_as_dict(dict(value))
 
-    return device_data
-
-
-def _regdata_as_dict(device_data: dict[str, Any]) -> dict[str, Any]:
-    """Represent regulator data as dictionary."""
-    if ATTR_REGDATA in device_data:
-        device_data[ATTR_REGDATA] = device_data[ATTR_REGDATA].data
+        if isinstance(value, EventManager):
+            device_data[key] = _device_data_as_dict(dict(value.data))
 
     return device_data
 
@@ -74,7 +59,5 @@ async def async_get_config_entry_diagnostics(
         "pyplumio": {
             "version": pyplumio_version,
         },
-        "data": _redact_device_data(
-            _regdata_as_dict(_sub_devices_as_dict(dict(device.data)))
-        ),
+        "data": _redact_device_data(_device_data_as_dict(dict(device.data))),
     }
